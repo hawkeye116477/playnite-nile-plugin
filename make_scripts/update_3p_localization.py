d51dd49949712ea38b87124d55a94b2ce63755c7
hawkeye116477 <hawkeye116477@gmail.com>
2026-09-12 16:04:20 +0200

    Restore missing loc

--- make_scripts/update_3p_localization.py
+++ make_scripts/update_3p_localization.py
@@ -66,11 +66,11 @@ for filename in os.listdir(pj(main_path, "..", "PlayniteExtensions", "PlayniteRe
     xml_doc = ET.ElementTree(xml_root)
 
     new_filename = filename
-    if filename not in ["LocSource.xaml", "LocalizationKeys.cs", "locstatus.json"]:
-        if filename == "en_US.xaml":
-            new_filename = "LocSource.xaml"
+    if filename not in ["LocalizationKeys.cs", "locstatus.json"]:
+        if filename == "LocSource.xaml":
+            new_filename = "en_US.xaml"
         playnite_loc = ET.parse(pj(main_path, "..", "PlayniteExtensions",
-                                "PlayniteRepo", "source", "Playnite", "Localization", new_filename))
+                                "PlayniteRepo", "source", "Playnite", "Localization", filename))         
         for child in playnite_loc.getroot():
             key = child.get(ET.QName(xmlns_x, "Key"))
             if key in playnite_loc_keys:
@@ -83,10 +83,9 @@ for filename in os.listdir(pj(main_path, "..", "PlayniteExtensions", "PlayniteRe
                 if key_text != "":
                     xml_root.append(new_key)
 
-    if filename not in ["LocSource.xaml", "LocalizationKeys.cs", "locstatus.json"]:
-        loc_sub_dir = filename.replace("_", "-").replace(".xaml", "")
+        loc_sub_dir = new_filename.replace("_", "-").replace(".xaml", "")
         amazon_file_path = pj(main_path, "..", "PlayniteExtensions",
-                            "source", "Libraries", "AmazonGamesLibrary", "Localization", filename)
+                            "source", "Libraries", "AmazonGamesLibrary", "Localization", new_filename)
         if os.path.isfile(amazon_file_path):
             amazon_loc = ET.parse(amazon_file_path)
             for child in amazon_loc.getroot():
