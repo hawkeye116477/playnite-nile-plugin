6cefb95e5b344ee5271726e0d759508fb8be0c5d
hawkeye116477 <hawkeye116477@gmail.com>
2026-09-12 16:35:36 +0200

    Improve metadata
    * Fix pcgamingwiki link
    * Add loading icon from exe

--- src/AmazonGamesMetadataProvider.cs
+++ src/AmazonGamesMetadataProvider.cs
@@ -2,6 +2,8 @@
 using Playnite.SDK.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace NileLibraryNS
 {
@@ -23,7 +25,26 @@ namespace NileLibraryNS
                 game.Name = game.Name.TrimEndString("CE") + "Collector's Edition";
             }
 
-            gameInfo.Links.Add(new Link("PCGamingWiki", @"http://pcgamingwiki.com/w/index.php?search=" + game.Name));
+            gameInfo.Links.Add(new Link("PCGamingWiki", $"http://pcgamingwiki.com/w/index.php?search=" + Uri.EscapeDataString(game.Name)));
+
+            // Load icon from exe
+            if (game.IsInstalled && string.IsNullOrEmpty(game.Icon))
+            {
+                var installedAppList = Nile.GetInstalledAppList();
+                var installedInfo = installedAppList.FirstOrDefault(i => i.id == game.GameId);
+                if (installedInfo != null)
+                {
+                    var gameConfig = Nile.GetGameConfiguration(installedInfo.path);
+                    if (!gameConfig.Main.Command.IsNullOrEmpty())
+                    {
+                        var exePath = Path.Combine(installedInfo.path, gameConfig.Main.Command);
+                        if (File.Exists(exePath))
+                        {
+                            gameInfo.Icon = new MetadataFile(exePath);
+                        }
+                    }
+                }
+            }
             return gameInfo;
         }
     }
